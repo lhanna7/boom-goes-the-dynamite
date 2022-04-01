127b3d78e94d2880eeb6ff520541fc963f008ec6
Lauren <lhanna7@ford.com>
2022-04-01 09:54:01 -0700

    BOOM

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function boom(){
+// function boom() {
+//   document.querySelector("#message").textContent = "BOOM"
+// }
+
+const button = document.querySelector("button")
+// button.addEventListener("click", boom)
+button.addEventListener("click", () => {
   document.querySelector("#message").textContent = "BOOM"
-}
+})
